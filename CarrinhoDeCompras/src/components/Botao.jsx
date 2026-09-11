8cfb96cb6ea5d9afdb6d0a2df87c12ef01d018ba
ataidesBergamin <ataidesnatabergamin@gmail.com>
2026-09-10 22:56:25 -0300

    commit 18 - feature/acessibilidade refatoração style componente Botao

--- CarrinhoDeCompras/src/components/Botao.jsx
+++ CarrinhoDeCompras/src/components/Botao.jsx
@@ -1,6 +1,8 @@
+import "./Botao.css";
+
 function Botao({ onClick, children, type = "button", disabled = false }) {
   return (
-    <button type={type} onClick={onClick} disabled={disabled}>
+    <button className="botao" type={type} onClick={onClick} disabled={disabled}>
       {children}
     </button>
   );
