b83e8108fee252f491e77903fa5d91f4f86c1ff5
ataidesBergamin <ataidesnatabergamin@gmail.com>
2026-09-10 21:41:38 -0300

    commit 15 - feature/acessibilidade refaturação  estilo da pagina de Pagamento com acessibilidade

--- CarrinhoDeCompras/src/components/Botao.jsx
+++ CarrinhoDeCompras/src/components/Botao.jsx
@@ -1,6 +1,6 @@
-function Botao({ onClick, children, type = "button" }) {
+function Botao({ onClick, children, type = "button", disabled = false }) {
   return (
-    <button type={type} onClick={onClick}>
+    <button type={type} onClick={onClick} disabled={disabled}>
       {children}
     </button>
   );
